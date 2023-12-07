e4d9d3eca2c1edc7fddbf009798c095a6be1ace3
Aria008 <106798911+Aria008@users.noreply.github.com>
2023-12-07 17:54:52 +0800

    Update Register.js

--- routes/Register.js
+++ routes/Register.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { User, Comment } = require("../models/forum.js");
 const app = express();
+const bcrypt = require("bcrypt");
 
 router.get("/", (req, res) => {
   res.render("Register");
@@ -10,20 +11,25 @@ router.get("/", (req, res) => {
 router.post("/register", async (req, res) => {
   try {
     const { profile_photo, full_name, user_name, email, password } = req.body;
-
+      bcrypt.hash(password, 10, async (err, hashedPassword) => {
+          if (err) {
+              console.error('Error occurred while hashing password:', err);
+              // Handle error
+          }
     // Create a new instance of the User
     const newUser = new User({
       profile_photo: profile_photo,
       full_name: full_name,
       user_name: user_name,
       email: email,
-      password: password,
+      password: hashedPassword,
     });
 
     await newUser.save();
 
     res.json({ message: "User created" });
-  } catch (error) {
+  }) 
+}catch (error) {
     console.error("Error creating user:", error);
     res.status(500).json({ error: "Failed to create user" });
   }
@@ -39,13 +45,13 @@ router.route("/user/:id").get(async (req, res) => {
   }
 });
 
-router.route("/users").get(async (req, res) => {
+router.route("/user/:id").get(async (req, res) => {
   try {
-    const users = await User.find();
-    res.json(users);
+    const fetchedUser = await User.findById(req.params.id);
+    res.json(fetchedUser);
   } catch (error) {
-    console.error("Error fetching users:", error);
-    res.status(500).json({ error: "Failed to fetch users" });
+    console.error("Error fetching user:", error);
+    res.status(500).json({ error: "Failed to fetch user" });
   }
 });
 
